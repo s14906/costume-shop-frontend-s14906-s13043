06a446b70f8bfbca9e61456cf496dced83b84dea
Pawel Bochnacki <you@example.com>
2023-09-05 21:25:27 +0200

    Item category screen improvements -adding a new item category functionality

--- src/app/ui/item/item-category-list/item-category-list.component.ts
+++ src/app/ui/item/item-category-list/item-category-list.component.ts
@@ -2,7 +2,12 @@ import {Component, OnDestroy} from '@angular/core';
 import {HttpService} from "../../../core/service/http/http.service";
 import {Subscription} from "rxjs";
 import {ItemCategoryDTO} from "../../../shared/models/dto.models";
-import {ItemResponse} from "../../../shared/models/rest.models";
+import {ItemResponse, SimpleResponse} from "../../../shared/models/rest.models";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {FormValidationService} from "../../../core/service/form/form-validation.service";
+import {SnackbarService} from "../../../core/service/snackbar.service";
+import {Router} from "@angular/router";
+import {HttpErrorService} from "../../../core/service/http/http-error.service";
 
 @Component({
   selector: 'app-item-category',
@@ -15,8 +20,18 @@ export class ItemCategoryListComponent implements OnDestroy {
   itemCategories: ItemCategoryDTO[] = [];
   currentPage: number = 0;
   itemsPerPage: number = 10;
-
-  constructor(private httpService: HttpService) {
+  addCategoryForm: FormGroup;
+  constructor(private httpService: HttpService,
+              private formBuilder: FormBuilder,
+              private formValidationService: FormValidationService,
+              private snackbarService: SnackbarService,
+              private httpErrorService: HttpErrorService,
+              private router: Router) {
+    this.addCategoryForm = this.formBuilder.group({
+        category: ['', Validators.required, this.formValidationService.validateFieldNotEmpty]
+      },
+      {updateOn: "submit"}
+    );
 
     this.allSubscriptions.push(
       this.httpService.getAllItemCategories().subscribe((next: ItemResponse): void => {
@@ -31,8 +46,23 @@ export class ItemCategoryListComponent implements OnDestroy {
     return this.itemCategories.slice(begin, end);
   }
 
-  addNewCategory(): void {
-
+  onSubmitAddNewCategory(): void {
+    if (this.formValidationService.isFormValid(this.addCategoryForm)) {
+      this.allSubscriptions.push(
+        this.httpService.postAddItemCategory(this.formValidationService.getFieldValue(this.addCategoryForm, 'category'))
+          .subscribe({
+            next: (next: SimpleResponse): void => {
+              this.snackbarService.openSnackBar(next.message);
+              this.router.navigateByUrl('/', {skipLocationChange: true}).then((): void => {
+                this.router.navigate(['item-categories']);
+              });
+            },
+            error: err => {
+              this.httpErrorService.handleError(err);
+            }
+          })
+      );
+    }
   }
 
   ngOnDestroy(): void {
